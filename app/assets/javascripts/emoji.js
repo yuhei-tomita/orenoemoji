e0987f899b6cd00eccf7c18099fb7df5cb835d74
Yuhei Tomita <you@example.com>
2019-05-17 16:45:56 +0900

    setting username

--- app/assets/javascripts/emoji.js
+++ app/assets/javascripts/emoji.js
@@ -6,13 +6,13 @@ $(document).on('turbolinks:load', function(){ //リロードしなくてもjsが
       url: '/emojis/search', //urlを指定
       type: 'GET', //メソッドを指定
       data: ('keyword=' + input), //コントローラーに渡すデータを'keyword=input(入力された文字のことですね)'にするように指定
-      processData: false, //おまじない
-      contentType: false, //おまじない
+      processData: false, //オブジェクトをクエリ文字列に変換するかどうかの設定
+      contentType: false, //
       dataType: 'json' //データ形式を指定
     })
-    .done(function(data){ //データを受け取ることに成功したら、dataを引数に取って以下のことする(dataには@usersが入っている状態ですね)
+    .done(function(data){ //データを受け取ることに成功したら、dataを引数に取って以下のことする(dataには@emojisが入る)
       $('#result').find('li').remove();  //idがresultの子要素のliを削除する
-      $(data).each(function(i, emoji){ //dataをuserという変数に代入して、以下のことを繰り返し行う(単純なeach文ですね)
+      $(data).each(function(i, emoji){ //dataをemojiという変数に代入して、以下のことを繰り返す
         $('#result').append(
           '<li class = "emoji '+ emoji.name + '">' +
           '<a href="'+ emoji.image.url +'" class="downloader"  download = "' + emoji.name +
@@ -25,7 +25,7 @@ $(document).on('turbolinks:load', function(){ //リロードしなくてもjsが
           '</div>' +
           '</a>' +
           '</li>' )
-           //resultというidの要素に対して、<li>ユーザーの名前</li>を追加する。
+           //resultというidの要素に対して、<li class ="emoji">...</li>を追加する。
   });
 });
   });
