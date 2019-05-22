a1effd0002d71f8fadae6bfc6f22d288e6742747
Yuhei Tomita <tomita.yuhei@moneyforward.co.jp>
2019-05-22 17:31:58 +0900

    application.jsを修正

--- app/assets/javascripts/channels/download.js
+++
@@ -1,9 +0,0 @@
-$scope.downLoadJson = function () {
-          //ファイルを作ってダウンロードします。
-          var resultJson = JSON.stringify($scope.jsonObj);
-          var downLoadLink = document.createElement("a");
-          downLoadLink.download = $scope.fileName;
-          downLoadLink.href = URL.createObjectURL(new Blob([resultJson], {type: "text.plain"}));
-          downLoadLink.dataset.downloadurl = ["text/plain", downLoadLink.download, downLoadLink.href].join(":");
-          downLoadLink.click();
-      }
