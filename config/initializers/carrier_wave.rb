20a66bd78a4e1a26fc0dadece198d02a9b4a2463
Yuhei Tomita <tomita.yuhei@moneyforward.co.jp>
2019-05-27 15:32:13 +0900

    carrieer_wave.rbの修正

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -8,6 +8,9 @@ if Rails.env.production?
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
+
+    config.fog_public = true
+
     config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
