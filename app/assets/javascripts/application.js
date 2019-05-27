24d856c83bf70bf1b5c2dad5439fb11656a5155d
Yuhei Tomita <tomita.yuhei@moneyforward.co.jp>
2019-05-27 15:38:20 +0900

    画像投稿時に画像のプレビューを表示

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,28 @@
 //= require tag-it
 //= require turbolinks
 //= require_tree .
+
+
+$(function(){
+  $fileField = $('#file')
+
+  // 選択された画像を取得し表示
+  $($fileField).on('change', $fileField, function(e) {
+    file = e.target.files[0]
+    reader = new FileReader(),
+    $preview = $("#img_field");
+
+    reader.onload = (function(file) {
+      return function(e) {
+        $preview.empty();
+        $preview.append($('<img>').attr({
+          src: e.target.result,
+          width: "100%",
+          class: "preview",
+          title: file.name
+        }));
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
+});
